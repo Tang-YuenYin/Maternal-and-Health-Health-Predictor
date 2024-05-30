988e4c828064bf9ef70278c1bb3b78c21fb54637
YuenYin <yuenyin.tang@gmail.com>
2024-05-31 00:38:23 +0800

    Update Log Book

--- app/pageFlip.js
+++ app/pageFlip.js
@@ -1,15 +1,22 @@
-import { PageFlip } from 'page-flip';
+//import { PageFlip } from 'page-flip';
+
+//const pageFlip = new PageFlip(htmlParentElement, settings);
 
 document.addEventListener("DOMContentLoaded", () => {
-    const pageFlip = new PageFlip(document.getElementById('book'), {
-        width: 800, // required parameter - base page width
-        height: 600, // required parameter - base page height
+    const pageFlip = new St.PageFlip(document.getElementById('last-page').parentNode, {
+        width: 500, // required parameter - base page width
+        height: 650, // required parameter - base page height
+        showCover: true, // enable the cover page
     });
 
+    
+    const div = document.querySelectorAll('.my-page');
+    console.log(div);
     pageFlip.loadFromHTML(document.querySelectorAll('.my-page'));
+
 
     pageFlip.on('flip', (e) => {
         // callback code
-        alert(`Current page number: ${e.data}`);
+        console.log(`Current page number: ${e.data}`);
     });
 });
